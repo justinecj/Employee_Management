d0af00bf2b989918c8ab4fe8c35bde1faebd9818
justinecj <justinejose008@gmail.com>
2025-07-30 13:35:21 +0530

    employee_management

--- employee_management/employee_management/settings.py
+++ employee_management/employee_management/settings.py
@@ -146,13 +146,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = "static/"
-STATIC_URL = '/static/'
+# Development mode — use uncollected static files
+if os.getenv('STATIC_URL_LOCAL') == 'True':
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        BASE_DIR / 'frontend' / 'static',
+    ]
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
-# Optional if not using collectstatic for dev
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend' / 'static',
-]
+# Production mode — use collectstatic + NGINX
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
