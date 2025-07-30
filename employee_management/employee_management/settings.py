6e8f6703cf7f82bd5408a10f015c9895a7ce282f
justinecj <justinejose008@gmail.com>
2025-07-30 11:50:55 +0530

    employee_management

--- employee_management/employee_management/settings.py
+++ employee_management/employee_management/settings.py
@@ -87,13 +87,37 @@ WSGI_APPLICATION = "employee_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+
+if (os.getenv('USE_MYSQL')=='True'):
+    # Use the production database settings
+    print("Using MySQL")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': 'root',
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': '3306',
+        }
+    }
+else:
+    # Use local development database settings
+    print("Using Local Database")
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
